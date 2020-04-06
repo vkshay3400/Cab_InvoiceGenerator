11917904bfb7075e5daade8be21d2b3b66a92b31
vkshay3400 <akshayp2208@gmail.com>
2020-04-06 11:09:01 +0530

    Added: Method for multi-ride

--- src/main/java/com/cabinvoice/Fare.java
+++ src/main/java/com/cabinvoice/Fare.java
@@ -6,9 +6,11 @@ public class Fare {
     public static final double TRAVEL_COST_PER_MIN = 1;
     public static final int MINIMUM_TRAVEL_COST = 5;
 
-    //METHOD TO GET FARE
-    public double getFare(double distancePerKm, int travelTimeMin){
-        double fare = distancePerKm * DISTANCE_COST_PER_KM + travelTimeMin * TRAVEL_COST_PER_MIN;
+    //METHOD TO GET TOTAL FARE
+    public double getFare(Rides[] multiRides) {
+        double fare = 0;
+        for (Rides ride : multiRides)
+            fare += ride.journeyInDistance * DISTANCE_COST_PER_KM + ride.journeyInTime * TRAVEL_COST_PER_MIN;
         return Math.max(MINIMUM_TRAVEL_COST, fare);
     }
 
---
+++ src/main/java/com/cabinvoice/Rides.java
@@ -0,0 +1,13 @@
+package com.cabinvoice;
+
+public class Rides {
+    //VARIABLES
+    public double journeyInDistance;
+    public int journeyInTime;
+
+    //METHOD TO GET RIDES
+    public Rides(double journeyInDistance, int journeyInTime) {
+        this.journeyInDistance = journeyInDistance;
+        this.journeyInTime = journeyInTime;
+    }
+}
