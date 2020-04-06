2f0b1515503953af03cb5cb1f13f3a7c59adf4e4
vkshay3400 <akshayp2208@gmail.com>
2020-04-06 10:30:19 +0530

    Refactor: To get minimum fare

--- src/main/java/com/cabinvoice/Fare.java
+++ src/main/java/com/cabinvoice/Fare.java
@@ -3,12 +3,13 @@ package com.cabinvoice;
 public class Fare {
     //CONSTANTS
     public static final double DISTANCE_COST_PER_KM = 10;
-    public static final double TRAVEL_COST_PER_MIN = 10;
+    public static final double TRAVEL_COST_PER_MIN = 1;
+    public static final int MINIMUM_TRAVEL_COST = 5;
 
     //METHOD TO GET FARE
     public double getFare(double distancePerKm, int travelTimeMin){
         double fare = distancePerKm * DISTANCE_COST_PER_KM + travelTimeMin * TRAVEL_COST_PER_MIN;
-        return fare;
+        return Math.max(MINIMUM_TRAVEL_COST, fare);
     }
 
     //MAIN METHOD
--- src/test/java/FareTest.java
+++ src/test/java/FareTest.java
@@ -8,6 +8,12 @@ public class FareTest {
     @Test
     public void givenDistanceANdTime_ShouldReturnFareForJourney() {
         double totalFare = fare.getFare(15, 14);
-        Assert.assertEquals(290, totalFare, 0);
+        Assert.assertEquals(164, totalFare, 0);
+    }
+
+    @Test
+    public void givenDistanceANdTime_ShouldReturnMinimumFareForJourney() {
+        double totalFare = fare.getFare(0.3, 1);
+        Assert.assertEquals(5, totalFare, 0);
     }
 }
