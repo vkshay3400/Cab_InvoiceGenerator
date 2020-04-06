4e467ae29cf84aabe474a9f2b46c8f80c4e85d11
vkshay3400 <akshayp2208@gmail.com>
2020-04-06 11:09:56 +0530

    Added: Test cases for multi ride

--- src/test/java/FareTest.java
+++ src/test/java/FareTest.java
@@ -1,4 +1,5 @@
 import com.cabinvoice.Fare;
+import com.cabinvoice.Rides;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -6,14 +7,23 @@ public class FareTest {
     Fare fare = new Fare();
 
     @Test
-    public void givenDistanceANdTime_ShouldReturnFareForJourney() {
-        double totalFare = fare.getFare(15, 14);
-        Assert.assertEquals(164, totalFare, 0);
+    public void givenDistanceAndTime_ShouldReturnFare_ForJourney() {
+        Rides[] rides = {new Rides(10.43, 27)};
+        double totalFare = fare.getFare(rides);
+        Assert.assertEquals(131.3, totalFare, 0);
     }
 
     @Test
-    public void givenDistanceANdTime_ShouldReturnMinimumFareForJourney() {
-        double totalFare = fare.getFare(0.3, 1);
+    public void givenDistanceAndTime_ShouldReturnMinimumFare_ForJourney() {
+        Rides[] rides = {new Rides(0.3, 1)};
+        double totalFare = fare.getFare(rides);
         Assert.assertEquals(5, totalFare, 0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare_ForJourney() {
+        Rides[] rides = {new Rides(40.2,23), new Rides(5.9, 10)};
+        double totalFare = fare.getFare(rides);
+        Assert.assertEquals(494.0, totalFare, 0);
     }
 }
