6d450773e25536d94e98274f19453120c2bba63e
vkshay3400 <akshayp2208@gmail.com>
2020-04-07 17:01:45 +0530

    Refactor: Changes in test cases and added ride type

--- src/test/java/FareTest.java
+++ src/test/java/FareTest.java
@@ -1,12 +1,14 @@
 import com.cabinvoice.Fare;
 import com.cabinvoice.Invoice;
+import com.cabinvoice.RideType;
 import com.cabinvoice.Rides;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class FareTest {
-    Fare fare = null;
+    Fare fare;
+    double totalFare;
 
     @Before
     public void setFare() throws Exception {
@@ -14,48 +16,82 @@ public class FareTest {
     }
 
     @Test
-    public void givenDistanceAndTime_ShouldReturnFare_ForJourney() {
-        Rides[] rides = {new Rides(10.43, 27)};
-        double totalFare = fare.getFare(rides);
+    public void givenDistanceAndTime_NormalRides_ShouldReturnFare_ForJourney() {
+        totalFare = fare.getFare(RideType.NORMAL_RIDES, 10.43, 27);
         Assert.assertEquals(131.3, totalFare, 0.0);
     }
 
     @Test
-    public void givenDistanceAndTime_ShouldReturnMinimumFare_ForJourney() {
-        Rides[] rides = {new Rides(0.3, 1)};
-        double totalFare = fare.getFare(rides);
+    public void givenDistanceAndTime_PremiumRides_ShouldReturnFare_ForJourney() {
+        totalFare = fare.getFare(RideType.PREMIUM_RIDES, 10.43, 27);
+        Assert.assertEquals(210.45, totalFare, 0.0);
+    }
+
+    @Test
+    public void givenDistanceAndTime_NormalRides_ShouldReturnMinimumFare_ForJourney() {
+        totalFare = fare.getFare(RideType.NORMAL_RIDES, 0.3, 1);
         Assert.assertEquals(5, totalFare, 0.0);
     }
 
     @Test
-    public void givenMultipleRides_ShouldReturnTotalFare_ForJourney() {
-        Rides[] rides = {new Rides(40.2, 23),
-                         new Rides(5.9, 10)
-        };
-        double totalFare = fare.getFare(rides);
-        Assert.assertEquals(494.0, totalFare, 0.0);
+    public void givenDistanceAndTime_PremiumRides_ShouldReturnMinimumFare_ForJourney() {
+        totalFare = fare.getFare(RideType.PREMIUM_RIDES, 0.3, 1);
+        Assert.assertEquals(20, totalFare, 0.0);
     }
 
     @Test
-    public void givenMultipleRides_ShouldReturnInvoice_ForJourney() {
-        Rides[] rides = {new Rides(40.2, 23),
-                         new Rides(5.9, 10)
-        };
-        Invoice invoiceActual = fare.getInvoice(rides);
+    public void givenMultipleRides_NormalRides_ShouldReturnTotalFare_ForJourney() {
+        Rides[] rides = {new Rides(RideType.NORMAL_RIDES, 40.2, 23),
+                         new Rides(RideType.NORMAL_RIDES, 5.9, 10)
+                        };
+        Invoice invoice = fare.invoiceGenerator(rides);
+        Assert.assertEquals(494.0, invoice.totalFare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_PremiumRides_ShouldReturnTotalFare_ForJourney() {
+        Rides[] rides = {new Rides(RideType.PREMIUM_RIDES, 40.2, 23),
+                         new Rides(RideType.PREMIUM_RIDES, 5.9, 10)
+                        };
+        Invoice invoice = fare.invoiceGenerator(rides);
+        Assert.assertEquals(757.5, invoice.totalFare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_NormalRides_ShouldReturnInvoice_ForJourney() {
+        String userId = "ABC";
+        Rides[] rides = {new Rides(RideType.NORMAL_RIDES, 40.2, 23),
+                         new Rides(RideType.NORMAL_RIDES, 5.9, 10)
+                        };
+        fare.addRides(userId, rides);
+        Invoice invoiceActual = fare.getInvoice(userId);
         Invoice invoiceExpected = new Invoice(2, 494.0);
         Assert.assertEquals(invoiceExpected, invoiceActual);
     }
 
     @Test
-    public void givenMultipleRides_ShouldReturnInvoiceSummary_ForJourney() {
-        Rides[] rides = {new Rides(40.2, 23),
-                         new Rides(5.9, 10),
-                         new Rides(27.0, 19),
-                         new Rides(45.5, 34),
-        };
-        fare.addRides("user1", rides);
-        Invoice invoiceActual = fare.getInvoice(rides);
-        Invoice invoiceExpected = new Invoice(4, 1272.0);
+    public void givenMultipleRides_PremiumRides_ShouldReturnInvoice_ForJourney() {
+        String userId = "ABC";
+        Rides[] rides = {new Rides(RideType.PREMIUM_RIDES, 40.2, 23),
+                         new Rides(RideType.PREMIUM_RIDES, 5.9, 10)
+                        };
+        fare.addRides(userId, rides);
+        Invoice invoiceActual = fare.getInvoice(userId);
+        Invoice invoiceExpected = new Invoice(2, 757.5);
+        Assert.assertEquals(invoiceExpected, invoiceActual);
+    }
+
+    @Test
+    public void givenMultipleRides_NormalPremiumRides_ShouldReturnInvoice_ForJourney() {
+        String userId = "ABC";
+        Rides[] rides = {new Rides(RideType.NORMAL_RIDES, 40.2, 23),
+                         new Rides(RideType.PREMIUM_RIDES, 5.9, 10),
+                         new Rides(RideType.NORMAL_RIDES, 27.0, 19),
+                         new Rides(RideType.PREMIUM_RIDES, 45.5, 34),
+                        };
+        fare.addRides(userId, rides);
+        Invoice invoiceActual = fare.getInvoice(userId);
+        Invoice invoiceExpected = new Invoice(4, 1573);
         Assert.assertEquals(invoiceExpected, invoiceActual);
     }
 }
